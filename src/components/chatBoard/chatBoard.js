3e5168fffa64b7cff1696f751ea001cd139cffdc
Vladislav <vladdrobovich@gmail.com>
2019-06-20 12:20:52 +0300

    style: change messages style

--- src/components/chatBoard/chatBoard.js
+++ src/components/chatBoard/chatBoard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import './chatBoard.css'
 
@@ -6,44 +7,36 @@ import Tab from 'react-bootstrap/Tab';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col'
 import ListGroup from 'react-bootstrap/ListGroup'
-import Button from "react-bootstrap/Button";
 
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
-
-
-
 
 
 function ChatBoard(props) {
     return (
         <Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1">
             <Row>
-                <Col sm={4}>
+                <Col sm={{span: 6, offset: 3}}>
                     <ListGroup variant="flush">
-                        <ListGroup.Item action>
+                        <ListGroup.Item>
                             <Fab size="small" color="primary" aria-label="Add">
                                 <AddIcon/>
                             </Fab>
                             <span className='newRoomSpan'>New room</span>
                         </ListGroup.Item>
-                        <ListGroup.Item action href="#link1">
-                            Link 1
+                        <ListGroup.Item action>
+                            <div>123</div>
+                            <div>123</div>
+                            <div>123</div>
+                            <div>123</div>
                         </ListGroup.Item>
-                        <ListGroup.Item action href="#link2">
+                        <ListGroup.Item action>
                             Link 2
                         </ListGroup.Item>
+                        <ListGroup.Item action>
+                            Link 3
+                        </ListGroup.Item>
                     </ListGroup>
-                </Col>
-                <Col sm={8}>
-                    <Tab.Content>
-                        <Tab.Pane eventKey="#link1">
-                            <h1>Link #1</h1>
-                        </Tab.Pane>
-                        <Tab.Pane eventKey="#link2">
-                            <h1>Link #2</h1>
-                        </Tab.Pane>
-                    </Tab.Content>
                 </Col>
             </Row>
         </Tab.Container>
@@ -51,3 +44,4 @@ function ChatBoard(props) {
 }
 
 export default ChatBoard;
+
